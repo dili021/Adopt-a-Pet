6485a5d6ab63c6a23b0cdd41add851a0b48d4fcb
stefan dili <stefan.dili.021@gmail.com>
2020-09-22 21:00:32 +0200

    added scripts and deps

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, name),
-    React.createElement('h2', {}, animal),
-    React.createElement('h2', {}, breed),
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
   ]);
 };
 
 const App = () => {
-  return React.createElement('div', { id: 'test-id' }, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet, { name: 'bobi', animal: 'ker', breed: 'dzukela' }),
-    React.createElement(Pet, { name: 'leo', animal: 'ker', breed: 'pudla' }),
-    React.createElement(Pet, { name: 'kurac', animal: 'ker', breed: 'kurac' }),
+  return React.createElement("div", { id: "test-id" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, { name: "bobi", animal: "ker", breed: "dzukela" }),
+    React.createElement(Pet, { name: "leo", animal: "ker", breed: "pudla" }),
+    React.createElement(Pet, { name: "kurac", animal: "ker", breed: "kurac" }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.querySelector('#root'));
+ReactDOM.render(React.createElement(App), document.querySelector("#root"));
